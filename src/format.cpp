1317bd574ac68f8fac7d8ad86bce74c65bf3a9b8
khaled747 <55651560+khaled747@users.noreply.github.com>
2020-09-18 22:06:05 +0200

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,14 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+  long HH{seconds/3600};
+  long H_{seconds%3600};
+  long MM{H_/60};
+  long SS{H_%60};
+  string HH_{std::to_string(HH)};
+  string MM_{std::to_string(MM)};
+  string SS_{std::to_string(SS)};
+  string time{HH_+':'+MM_+':'+SS_};
+  return time;
+}
