f7d611a9103770c4afa0f0044aa82ee54b26f763
khaled747 <55651560+khaled747@users.noreply.github.com>
2020-09-18 22:09:10 +0200

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,29 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  
+  std::vector<std::string> fies_list = LinuxParser::CpuUtilization();
+  
+  for (size_t i{0}; i<fies_list.size();i++){
+    if (i!=LinuxParser::kGuest_ and i!=LinuxParser::kGuestNice_){
+    	total_fies += std::stof(fies_list[i]);
+    }
+    if (i==LinuxParser::kIdle_ or i==LinuxParser::kIOwait_){
+    	idle_fies += std::stof(fies_list[i]);
+    }
+  }
+  util_fies = total_fies - idle_fies;
+  
+  bool curr_util = true;
+  if (curr_util){
+   usage = (util_fies-util_fies_pre)/(total_fies-total_fies_pre);
+    total_fies_pre = total_fies;
+    util_fies_pre = util_fies;
+  }
+  else{
+    usage = util_fies/total_fies;
+  }
+  
+  return usage; 
+}}
